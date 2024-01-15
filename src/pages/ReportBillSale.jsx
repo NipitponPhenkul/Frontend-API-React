295b476f7e47fbd8cd08312a8efa815ba141b1f5
NipitponPhenkul <nipitponphenkul@hotmail.com>
2024-01-15 20:44:44 +0700

    update bill details modal

--- src/pages/ReportBillSale.jsx
+++ src/pages/ReportBillSale.jsx
@@ -4,12 +4,16 @@ import Swal from 'sweetalert2';
 import axios from 'axios';
 import Config from '../config';
 import * as dayjs from 'dayjs';
+import Modal from '../components/Modal';
 
 function ReportBillSale() {
     const myDate = new Date();
     const y = myDate.getFullYear();
     const m = (myDate.getMonth() + 1).toString().padStart(2, '0');
     const d = myDate.getDate().toString().padStart(2, '0');
+
+    const [billSaleDetails, setBillSaleDetails] = useState([]);
+    const [sumBill, setSumBill] = useState(0);
 
     const [fromDate, setFromDate] = useState(() => {
         return `${y}-${m}-01`;
@@ -38,6 +42,31 @@ function ReportBillSale() {
             Swal.fire({
                 title: 'error',
                 text: error.message,
+                icon: 'error'
+            })
+        }
+    }
+
+    const handleShowDetail = async (item) => {
+        try {
+            await axios.get(Config.api + '/api/Report/BillSaleDetail/' + item.id, Config.headers).then(res => {
+                if (res.data.message === 'success') {
+                    setBillSaleDetails(res.data.results);
+                    let sum = 0;
+                    res.data.results.map(item => {
+                        const qty = item.qty;
+                        const price = item.price;
+                        sum += (qty * price)
+                    })
+                    setSumBill(sum);
+                }
+            }).catch(err => {
+                throw err.response.data;
+            }) 
+        } catch (e) {
+            Swal.fire({
+                title: 'error',
+                text: e.message,
                 icon: 'error'
             })
         }
@@ -83,7 +112,7 @@ function ReportBillSale() {
                                 { billSales.length > 0 ? billSales.map((item, index) =>
                                 <tr>
                                     <td>
-                                        <button className='btn btn-primary'>
+                                        <button data-toggle='modal' data-target='#modalBillSaleDetail' onClick={e => handleShowDetail(item)} className='btn btn-primary'>
                                             <i className='fa fa-file-alt mr-2'></i>
                                             Bill Details
                                         </button>
@@ -98,6 +127,36 @@ function ReportBillSale() {
                     </div>
                 </div>
             </Template>
+            <Modal id='modalBillSaleDetail' title='Bill Details' modalSize='modal-lg'>
+                <table className='table table-bordered table-striped'>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>ISBN</th>
+                            <th>List</th>
+                            <th className='text-right'>Price</th>
+                            <th className='text-right'>Quantity</th>
+                            <th className='text-right'>Amount</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { billSaleDetails.length > 0 ? billSaleDetails.map((item, index) =>
+                        <tr>
+                            <td>{index + 1}</td>
+                            <td>{item.isbn}</td>
+                            <td>{item.name}</td>
+                            <td className="text-right">{item.price.toLocaleString('th-TH')}</td>
+                            <td className="text-right">{item.qty}</td>
+                            <td className='text-right'>{(item.qty * item.price).toLocaleString('th-TH')}</td>
+                        </tr>
+                        ) : ''}
+                    </tbody>
+                </table>
+
+                <div className='mt-3 h5 text-right'>
+                    Bill Amount : {sumBill.toLocaleString('th-TH')}
+                </div>
+            </Modal>
         </>
     )
 }
