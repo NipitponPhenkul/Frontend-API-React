29b428583ac16be77c5d262a9d284059090bf3ed
NipitponPhenkul <nipitponphenkul@hotmail.com>
2024-01-25 12:24:48 +0700

    update table reportSumSalePerDay

--- src/pages/ReportSumSalePerDay.jsx
+++ src/pages/ReportSumSalePerDay.jsx
@@ -1,8 +1,16 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Template from '../components/Template';
+import Swal from 'sweetalert2';
+import axios from 'axios';
+import Config from '../config';
 
 function ReportSumSalePerDay() {
     const myDate = new Date();
+
+    const [days, setDays] = useState([]);
+    const [year, setYear] = useState(myDate.getFullYear());
+    const [month, setMonth] = useState(myDate.getMonth() + 1);
+
     const [arrYear, setArrYear] = useState(() => {
         let arr = [];
         let y = myDate.getFullYear();
@@ -31,6 +39,27 @@ function ReportSumSalePerDay() {
         }
         return newArr;
     })
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            axios.get(Config.api + '/api/Report/SumSalePerMonth/' + year + '/' + month, Config.headers).then(res => {
+                setDays(res.data.results);
+            }).catch(err => {
+                throw err.response.data;
+            })
+        } catch (e) {
+            Swal.fire({
+                title: 'error',
+                text: e.message,
+                icon: 'error'
+            })
+        }
+    }
+
     return (
         <>
             <Template>
@@ -42,7 +71,7 @@ function ReportSumSalePerDay() {
                                 <div className='col-4'>
                                     <div className='input-group'>
                                         <span className='input-group-text'>Year</span>
-                                        <select className='form-control'>
+                                        <select className='form-control' onChange={e => setYear(e.target.value)}>
                                             { arrYear.map(item => 
                                                 <option value={item.value} selected={item.selected}>
                                                     {item.value}
@@ -54,7 +83,7 @@ function ReportSumSalePerDay() {
                                 <div className='col-4'>
                                     <div className='input-group'>
                                         <span className='input-group-text'>Month</span>
-                                        <select className='form-control'>
+                                        <select className='form-control' onChange={e => setMonth(e.target.value)}>
                                             { arrMonth.map(item => 
                                                 <option value={item.monthIndex} selected={item.selected}>
                                                     {item.value}
@@ -64,13 +93,29 @@ function ReportSumSalePerDay() {
                                     </div>
                                 </div>
                                 <div className='col-4'>
-                                    <button className='btn btn-primary'>
+                                    <button onClick={fetchData} className='btn btn-primary'>
                                         <i className='fa fa-check mr-2'></i>
                                         Show
                                     </button>
                                 </div>
                             </div>
                         </div>
+                        <table className='table table-bordered table-striped mt-3'>
+                            <thead>
+                                <tr>
+                                    <th width='100px'>Date</th>
+                                    <th>Sales</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                { days.length > 0 ? days.map(item => 
+                                <tr>
+                                    <td>{item.day}</td>
+                                    <td className='text-right'>{item.totalSum.toLocaleString('th-TH')}</td>
+                                </tr>
+                                ) : ''}
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </Template>
